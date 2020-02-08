a00f009c2bfbbc7908370eab01bdd6876d0a5771
Md Obydullah <obydul@makpie.com>
2020-02-09 01:41:01 +0600

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+<p align="center">
+<img src="https://user-images.githubusercontent.com/13184472/74090933-c9abe380-4adb-11ea-82b7-94d84a5ae13d.png" width="400"><br>
+MyNotePaper.com Presents:<br>
+<a href="https://s.mynotepaper.com/y1E4" target="_blank">Auto Deploy Vue.js App to AWS S3 Using GitLab CI/CD</a>
+</p>
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="CI/CD" src="./assets/ci-cd-test-deploy-illustration.png" style="max-width: 500px;">
+    <p>
+      MyNotePaper.com Presents:<br><br>
+      <a href="https://s.mynotepaper.com/y1E4" target="_blank">Auto Deploy Vue.js App to AWS S3 Using GitLab CI/CD</a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
